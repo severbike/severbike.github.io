c5ae392717e634e2a2cd844e4b84832bc6ae1533
severbike <56751678+severbike@users.noreply.github.com>
2022-01-22 22:53:11 +0300

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,9 @@
 const pageUrl = document.location;
 const params = new URLSearchParams(pageUrl.search);
+const maxResults = params.get['max-results'];
+const startPost = params.get['start'];
+
+
 for (let p of params) {
 	console.log(p[0] + ' - ' + p[1]);
 }
