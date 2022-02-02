fa4f750c52beac589967a3fd2ec890f1dbc591a9
severbike <56751678+severbike@users.noreply.github.com>
2022-02-02 22:38:35 +0300

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,11 +1,34 @@
 //Page URL
-const pageUrl = new URL('https://solzaru.blogspot.com/search/label/%D0%9F%D0%BE%D1%85%D0%BE%D0%B4%D1%8B?max-results=10');
+const pageUrl = document.location;
 //Create URL Search Params
 const params = new URLSearchParams(pageUrl.search);
 //Проверяем, существует ли параметр start, если не существует, возвращаем 0
-const startPosts = params.has('start') ? params.get('start') : 0;
+const startPost = params.has('start') ? Number(params.get('start')) : 0;
+//Проверяем количество постов
+const maxResults = params.has('max-results') ? Number(params.get('max-results')) : 20;
+console.log('maxResults');
 //Извлекаем label из URL страницы
 const label =  pageUrl.pathname.toString().substring(pageUrl.pathname.lastIndexOf('/') + 1);
 //Формируем ссылку на RSS-ленту для label
 const rssUrl = pageUrl.origin + '/feeds/posts/summary/-/' + label + '?alt=json';
-console.log(rssUrl);
+
+//console.log(decodeURIComponent(rssUrl));
+//Запрашиваем RSS-ленту тега в JSON
+fetch(rssUrl)
+  .then((response) => {
+	//Преобразовываем ответ сервера в JSON-объект
+    return response.json();
+  })
+  .then((data) => {
+	//Обрабатываем JSON
+    navigation(data);
+  });
+ 
+function navigation(rj) {
+	console.log(rj.feed.entry.length);
+	console.log(startPosts);
+	let lastPost = startPost + maxResults;
+	if (Number(rj.feed.entry.length) < lastPos) {
+		console.log('Hide Next Link');
+	}
+}
