9e42eea41c2f3c40ada7bb21c2302e9179a68fbd
severbike <56751678+severbike@users.noreply.github.com>
2022-02-02 21:28:09 +0300

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,10 +1,11 @@
-const pageUrl = document.location;
+//Page URL
+const pageUrl = new URL('https://solzaru.blogspot.com/search/label/%D0%9F%D0%BE%D1%85%D0%BE%D0%B4%D1%8B?max-results=10');
+//Create URL Search Params
 const params = new URLSearchParams(pageUrl.search);
-const maxResults = params.get['max-results'];
-const startPost = params.get['start'];
-
-
-for (let p of params) {
-	console.log(p[0] + ' - ' + p[1]);
-}
-console.log(pageUrl);
+//Проверяем, существует ли параметр start, если не существует, возвращаем 0
+const startPosts = params.has('start') ? params.get('start') : 0;
+//Извлекаем label из URL страницы
+const label =  pageUrl.pathname.toString().substring(pageUrl.pathname.lastIndexOf('/') + 1);
+//Формируем ссылку на RSS-ленту для label
+const rssUrl = pageUrl.origin + '/feeds/posts/summary/-/' + label + '?alt=json';
+console.log(rssUrl);
