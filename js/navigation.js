6d95a0a14f52cc036fd3370d888272326890fb30
severbike <56751678+severbike@users.noreply.github.com>
2022-03-01 00:05:00 +0300

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -7,9 +7,9 @@ function sbModNavigation () {
 	const startPost = params.has('start') ? Number(params.get('start')) : 0;
 	//Проверяем количество постов
 	const maxResults = params.has('max-results') ? Number(params.get('max-results')) : 20;
-	console.log('maxResults='+maxResults );
-//Извлекаем label из URL страницы
-//const label =  pageUrl.pathname.toString().substring(pageUrl.pathname.lastIndexOf('/') + 1);
+	//Извлекаем label из URL страницы
+	const label =  pageUrl.pathname.toString().substring(pageUrl.pathname.lastIndexOf('/') + 1);
+	console.log(label);
 //Формируем ссылку на RSS-ленту для label.
 //const rssUrl = pageUrl.origin + '/feeds/posts/summary/-/' + label + '?alt=json';
 
