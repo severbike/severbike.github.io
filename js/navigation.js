3fe8a660ef5bd2a1c9ff0c976ddea70eff3a32f8
severbike <56751678+severbike@users.noreply.github.com>
2022-02-28 23:54:16 +0300

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,34 +1,34 @@
-function sbModNavigation () {
-	
+function sbModNavigation () {	
 	//Page URL
 	const pageUrl = document.location;
 	console.log(pageUrl);
-}
-
-
-//Create URL Search Params
-const params = new URLSearchParams(pageUrl.search);
+	//Create URL Search Params
+//const params = new URLSearchParams(pageUrl.search);
 //Проверяем, существует ли параметр start, если не существует, возвращаем 0
-const startPost = params.has('start') ? Number(params.get('start')) : 0;
+//const startPost = params.has('start') ? Number(params.get('start')) : 0;
 //Проверяем количество постов
-const maxResults = params.has('max-results') ? Number(params.get('max-results')) : 20;
-console.log('maxResults');
+//const maxResults = params.has('max-results') ? Number(params.get('max-results')) : 20;
+//console.log('maxResults');
 //Извлекаем label из URL страницы
-const label =  pageUrl.pathname.toString().substring(pageUrl.pathname.lastIndexOf('/') + 1);
+//const label =  pageUrl.pathname.toString().substring(pageUrl.pathname.lastIndexOf('/') + 1);
 //Формируем ссылку на RSS-ленту для label.
-const rssUrl = pageUrl.origin + '/feeds/posts/summary/-/' + label + '?alt=json';
+//const rssUrl = pageUrl.origin + '/feeds/posts/summary/-/' + label + '?alt=json';
 
 //console.log(decodeURIComponent(rssUrl));
 //Запрашиваем RSS-ленту тега в JSON
-fetch(rssUrl)
-  .then((response) => {
-	//Преобразовываем ответ сервера в JSON-объект
-    return response.json();
-  })
-  .then((data) => {
+//fetch(rssUrl)
+//  .then((response) => {
+//	//Преобразовываем ответ сервера в JSON-объект
+//    return response.json();
+//  })
+//  .then((data) => {
 	//Обрабатываем JSON
-    navigation(data);
-  });
+//    navigation(data);
+ // });
+}
+
+
+
  
 function navigation(rj) {
 	console.log(rj.feed.entry.length);
